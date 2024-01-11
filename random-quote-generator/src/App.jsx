66f33bee0ad554a4fc4f77b05693b1a8b440ccca
codebadger99 <ebukanuel2021@gmail.com>
2024-01-11 12:01:20 +0100

    updated

--- random-quote-generator/src/App.jsx
+++ random-quote-generator/src/App.jsx
@@ -4,21 +4,28 @@ import React, { useState, useEffect } from "react";
 export default function App() {
   const [Quote, setQuote] = useState("");
   const [Author, setAuthor] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     getQuotes();
   }, []);
 
   const getQuotes = () => {
-    fetch("https://dummyjson.com/quotes")
-      .then((response) => response.json())
-      .then((data) => {
-        let dataQuotes = data.quotes;
-        let randomnum = Math.floor(Math.random() * dataQuotes.length);
-        let randomQuotes = dataQuotes[randomnum];
-        setQuote(randomQuotes.quote);
-        setAuthor(randomQuotes.author);
-      });
+    try {
+      setLoading(true);
+      fetch("https://dummyjson.com/quotes")
+        .then((response) => response.json())
+        .then((data) => {
+          let dataQuotes = data.quotes;
+          let randomnum = Math.floor(Math.random() * dataQuotes.length);
+          let randomQuotes = dataQuotes[randomnum];
+          setQuote(randomQuotes.quote);
+          setAuthor(randomQuotes.author);
+          setLoading(false);
+        });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleclick = () => {
@@ -26,12 +33,31 @@ export default function App() {
   };
   return (
     <div className="App">
-      <div className="card">
-        <h1>{Quote}</h1> 
-         <span>{Author}</span>
-        <button className="btn" onClick={handleclick}>
-          new quote
-        </button>
+      <div className="container">
+        <section className="mx-auto my-5">
+          <div className="card">
+            <div className="card-body">
+              <blockquote className="blockquote blockquote-custom bg-white px-3 pt-4">
+                <div className="blockquote-custom-icon bg-info shadow-1-strong">
+                  <i className="fa fa-quote-left text-white"></i>
+                </div>
+                <p className="mb-0 mt-2 font-italic">{Quote}</p>
+                <footer className="blockquote-footer pt-4 mt-4 border-top">
+                  {Author}
+                </footer>
+              </blockquote>
+              <div className="d-flex align-items-center justify-content-center">
+                <button
+                  className="btn btn-primary mt-2"
+                  onClick={handleclick}
+                  disabled={isLoading}
+                >
+                  {isLoading ? "Loading..." : "More"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
